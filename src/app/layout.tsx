90d876ee16b0b501ee578243ba2f395e2f212053
Ynon Cohen <ynon@evermile.io>
2025-01-06 20:51:16 +0000

    favicon again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,55 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
     title: "Avoriaz",
     description: "Ski Trip planner for Avoriaz",
     icons: {
-        icon: '/favicon.ico',
-        shortcut: '/favicon.ico',
+        icon: [
+            {
+                url: '/avoriaz/favicon.ico',
+                sizes: 'any',
+            },
+            {
+                url: '/avoriaz/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png',
+            },
+            {
+                url: '/avoriaz/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png',
+            },
+        ],
+        shortcut: '/avoriaz/favicon.ico',
+        apple: '/avoriaz/favicon.ico',
     },
-};
+}
+
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
